b2c2ecfbf301e5884565736690282b6ee8ebed37
dradnats1012 <herjebi1012@gmail.com>
2025-04-25 15:33:45 +0900

    init

---
+++ src/main/java/com/example/bcsd/BcsdApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcsdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bcsd/BcsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
