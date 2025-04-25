0379815576fb7bb0cc352345caea414d4bc878f6
dradnats1012 <herjebi1012@gmail.com>
2025-04-25 16:11:00 +0900

    feat: 수업 실습

---
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.bcsd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!!!!!";
+    }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello";
+    }
+}
