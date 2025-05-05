b1d3ebef0bec8aa6538518af7078ed0e78f325d5
PARK TAEJIN <140797244+taejinn@users.noreply.github.com>
2025-05-05 22:44:26 +0900

    feat: /introduce 에 ?name=<이름> 을 이용해 이름 수정 가능하도록 추가

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.bcsd;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/introduce")
-    public String hello() {
+    public String hello(@RequestParam String name, Model model) {
+        model.addAttribute("name", name != "" ? name : "박태진");
         return "hello";
     }
 }
