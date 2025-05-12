5e122fd62da48da5576aa53289647e0d2c2cf84a
PARK TAEJIN <140797244+taejinn@users.noreply.github.com>
2025-05-12 20:09:36 +0900

    feat: /posts 추가

--- src/main/java/com/example/bcsd/ArticleController.java
+++ src/main/java/com/example/bcsd/ArticleController.java
@@ -4,18 +4,27 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class ArticleController {
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        List<Post> posts = Post.findAll();
+        model.addAttribute("posts", posts);
+        return "posts";
+    }
 
     @GetMapping("/article")
+    @ResponseBody
     public ResponseEntity<List<Post>> getArticles() {
         List<Post> posts = Post.findAll();
         if (posts.isEmpty()) {
@@ -25,6 +34,7 @@ public class ArticleController {
     }
 
     @GetMapping("/article/{id}")
+    @ResponseBody
     public ResponseEntity<Post> getArticle(@PathVariable("id") Integer id) {
         Post post = Post.findById(id);
         if (post == null) {
@@ -34,12 +44,14 @@ public class ArticleController {
     }
 
     @PostMapping("/article")
+    @ResponseBody
     public ResponseEntity<Post> createArticle(@RequestBody Post post) {
         post.save();
         return ResponseEntity.status(HttpStatus.CREATED).body(post);
     }
 
     @PutMapping("/article/{id}")
+    @ResponseBody
     public ResponseEntity<Post> updateArticle(@PathVariable("id") Integer id, @RequestBody Post requestPost) {
         Post post = Post.findById(id);
         if (post == null) {
@@ -54,6 +66,7 @@ public class ArticleController {
     }
 
     @DeleteMapping("/article/{id}")
+    @ResponseBody
     public ResponseEntity<Void> deleteArticle(@PathVariable("id") Integer id) {
         Post post = Post.findById(id);
         if (post == null) {
