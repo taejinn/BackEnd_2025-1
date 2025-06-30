c319849f952940a994079d4a7a50a1f8e8a11e5a
PARK TAEJIN <140797244+taejinn@users.noreply.github.com>
2025-06-30 18:49:07 +0900

    refactor: 'Lombok' IS GOD.

--- src/main/java/com/example/bcsd/dto/ArticleResponseDto.java
+++ src/main/java/com/example/bcsd/dto/ArticleResponseDto.java
@@ -11,6 +11,13 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArticleResponseDto {
     
     @NotNull
@@ -43,21 +50,6 @@ public class ArticleResponseDto {
     @PastOrPresent
     private LocalDateTime updatedAt;
 
-    public ArticleResponseDto() {
-    }
-
-    public ArticleResponseDto(Long id, Long memberId, Long boardId, String boardName, 
-                            String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.memberId = memberId;
-        this.boardId = boardId;
-        this.boardName = boardName;
-        this.title = title;
-        this.content = content;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
     public static ArticleResponseDto from(Article article) {
         Board board = article.getBoard();
         return new ArticleResponseDto(
@@ -70,69 +62,5 @@ public class ArticleResponseDto {
                 article.getCreatedAt(),
                 article.getUpdatedAt()
         );
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public Long getBoardId() {
-        return boardId;
-    }
-
-    public void setBoardId(Long boardId) {
-        this.boardId = boardId;
-    }
-
-    public String getBoardName() {
-        return boardName;
-    }
-
-    public void setBoardName(String boardName) {
-        this.boardName = boardName;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
     }
 } 
